ff34db61690c479a11670e9c1e3355071afd8f62
sujallmajhi <sujalmajhee103@gmail.com>
2025-01-03 10:08:46 +0545

    modified

--- new_year_notifier.py
+++ new_year_notifier.py
@@ -2,5 +2,14 @@ import time
 from datetime import datetime
 from plyer import notification
 
-today=datetime.today()
-print(today)
+while True:
+    today=datetime.today()  #today=take today date and time from the module
+    
+    if today.month==1 and today.day==1: #check if today is month 1 and day is day 1
+        notification.notify(
+            title="Happy new year",
+            message="wishing u a Happy new year",
+            timeout=10
+        )
+        
+        time.sleep(86400) #sleep for 24 hrs
